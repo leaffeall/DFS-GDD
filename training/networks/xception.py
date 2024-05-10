f2ed0048615d3ad93b2a03f78075c6fbb9879067
leaffeall <149876140+leaffeall@users.noreply.github.com>
2024-05-10 09:08:11 +0800

    Update xception.py

--- training/networks/xception.py
+++ training/networks/xception.py
@@ -1,12 +1,3 @@
-'''
-# author: Zhiyuan Yan
-# email: zhiyuanyan@link.cuhk.edu.cn
-# date: 2023-0706
-
-The code is mainly modified from GitHub link below:
-https://github.com/ondyari/FaceForensics/blob/master/classification/network/xception.py
-'''
-
 import logging
 
 
