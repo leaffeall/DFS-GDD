3ec5bc86d7b7a3acd025e567acf5cfc1a2aba537
leaffeall <149876140+leaffeall@users.noreply.github.com>
2024-11-30 09:51:56 +0800

    Delete training directory

--- training/detectors/__init__.py
+++
@@ -1,4 +0,0 @@
-from utils.registry import DETECTOR
-
-from .DFS_1 import DFS_1
-from .DFS_2 import DFS_2
--- training/loss/__init__.py
+++
@@ -1,4 +0,0 @@
-from utils.registry import LOSSFUNC
-from .cross_entropy_loss import CrossEntropyLoss
-from .contrastive_regularization import ContrastiveLoss
-from .l1_loss import L1Loss
